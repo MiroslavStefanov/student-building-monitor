96a9078f08320ee60b87288d8d62a4c9e4d19412
Kristian Dimitrov <kristiandimitrov12@gmail.com>
2021-06-13 21:04:29 +0300

    Creating a foundation project

--- View/src/assets/js/building-blocks/app-dashboard-layout.js
+++ View/src/assets/js/building-blocks/app-dashboard-layout.js
@@ -2,4 +2,3 @@ $('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
   e.preventDefault();
   $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
 });
-
